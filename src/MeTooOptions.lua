2d81f8b1f1d6075aa742ff3057f73a4b9f479d04
opussf <github@zz9-za.com>
2018-08-02 18:31:13 -0700

    Even less debug info

--- src/MeTooOptions.lua
+++ src/MeTooOptions.lua
@@ -24,7 +24,6 @@ function MeToo.OptionsPanel_Cancel()
 	--MeToo.Print( "Options_Panel_Cancel" )
 	if MeToo.oldValues then
 		for key,val in pairs( MeToo.oldValues ) do
-			print( key )
 			MeToo_options[key] = val
 		end
 	end
@@ -33,13 +32,12 @@ end
 function MeToo.OptionsPanel_Default()
 	--MeToo.Print("Default")
 	for k,v in pairs( MeToo.defaultOptions ) do
-		print( k )
 		MeToo_options[k] = v
 	end
 end
 function MeToo.OptionsPanel_Refresh()
 	-- set the drop down values here...  Maybe more?
-	MeToo.Print( "OptionsPanel_Refresh" )
+	--MeToo.Print( "OptionsPanel_Refresh" )
 	MeTooOptionsFrame_MountSuccessDoEmote:SetChecked( MeToo_options["mountSuccess_doEmote"] )
 	MeTooOptionsFrame_MountSuccessEmoteEditBox:SetText( MeToo_options["mountSuccess_emote"] )
 	MeTooOptionsFrame_MountSuccessEmoteEditBox:SetCursorPosition(0)
@@ -69,7 +67,6 @@ function MeToo.OptionsPanel_OnLoad( panel )
 	for k,v in pairs( MeToo.defaultOptions ) do
 		MeToo_options[k] = MeToo_options[k] or v
 	end
-	--MeToo_options = { unpack( MeToo.defaultOptions ) }
 end
 -----------------
 function MeToo.OptionsPanel_CheckButton_OnShow( self, option, text )
@@ -84,15 +81,12 @@ function MeToo.OptionsPanel_CheckButton_PostClick( self, option )
 	end
 	MeToo_options[option] = self:GetChecked()
 end
-
-
 ---------------
 function MeToo.OptionsPanel_EditBox_OnLoad( self, option )
 	--MeToo.Print( "EditBox_OnLoad( "..self:GetName()..", "..option.." )" )
 	self:SetAutoFocus( false )
 	--self:SetCursorPosition(0)
 end
-
 function MeToo.OptionsPanel_EditBox_TextChanged( self, option )
 	MeToo.Print( "TextChanged: "..option.." >"..self:GetText() )
 	if MeToo.oldValues then
@@ -102,51 +96,3 @@ function MeToo.OptionsPanel_EditBox_TextChanged( self, option )
 	end
 	MeToo_options[option] = self:GetText()
 end
-
-
---[[
-
-
-
-
-TextBox events:
-
-    OnCursorChanged
-    OnEditFocusGained
-    OnEditFocusLost
-    OnEnterPressed
-    OnEscapePressed
-    OnInputLanguageChanged
-    OnSpacePressed
-    OnTabPressed
-    OnTextChanged
-    OnTextSet
-
-
-
-
-
-
-function INEED.OptionsPanel_Refresh()
-	-- Called when options panel is opened.
---	INEEDOptionsFrame_ShowProgress:SetChecked(INEED_options["showProgress"])
---	INEEDOptionsFrame_AlertOnSuccess:SetChecked(INEED_options["showSuccess"])
---	INEEDOptionsFrame_AudibleAlert:SetChecked(INEED_options["audibleSuccess"])
---	INEEDOptionsFrame_DoEmote:SetChecked(INEED_options["doEmote"])
---	INEEDOptionsFrame_PlaySound:SetChecked(INEED_options["playSoundFile"])
-
-	INEEDOptionsFrame_DoEmoteEditBox:SetText(INEED_options["emote"])
-	INEEDOptionsFrame_PlaySoundEditBox:SetText(INEED_options["soundFile"])
-
-	--INEED.Print("Options Panel Refresh: "..INEED_options["emote"])
-end
-
-function INEED.OptionsPanel_EditBox_TextChanged( self, option )
-	if INEED.oldValues then
-		INEED.oldValues[option] = INEED.oldValues[option] or INEED_options[option]
-	else
-		INEED.oldValues={[option]=INEED_options[option] }
-	end
-	INEED_options[option] = self:GetText()
-end
-]]
